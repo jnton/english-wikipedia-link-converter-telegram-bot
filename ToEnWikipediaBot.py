a4cc769db315c4713187505ba6f8779284c30c72
JnTon <84038748+JnTon@users.noreply.github.com>
2024-09-15 17:42:14 +0200

    Update ToEnWikipediaBot.py

--- ToEnWikipediaBot.py
+++ ToEnWikipediaBot.py
@@ -245,28 +245,29 @@ def setup_handlers(application):
     application.add_handler(license_handler)
 
 async def license(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    license_text = """
-*License*
-
-The code in this repository is licensed under the [GNU Affero General Public License v3\\.0 \\(AGPL\\-3\\.0\\)](https://www.gnu.org/licenses/agpl-3.0.en.html), except where otherwise specified.
-
-The icon for the *English Wikipedia Link Converter* Telegram Bot is licensed under a [Creative Commons Attribution\\-ShareAlike 4\\.0 International License \\(CC BY\\-SA 4\\.0\\)](http://creativecommons.org/licenses/by-sa/4.0/). See the [icon directory](https://github.com/JnTon/English-Wikipedia-Link-Converter-Telegram-Bot/tree/main/Telegram-Bot-Icon) for more details.
-
-*Image Credits*
-
-The bot's icon incorporates images from the following sources:
-
-\- *Wikipedia logo*, Version2 by Vanished user 24kwjf10h32h, Version 1 by Nohat \\(concept by Paullusmagnus\\); Wikimedia., is used under a [Creative Commons Attribution\\-ShareAlike 3\\.0 Unported License \\(CC BY\\-SA 3\\.0\\)](https://creativecommons.org/licenses/by-sa/3.0/) and can be found [here on Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Wikipedia-logo-v2-square.svg).
-
-\- *Left arrow*, by Icons8 is licensed under [CC0](https://creativecommons.org/share-your-work/public-domain/cc0/) and is available [here on Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Left-arrow_%2861413%29_-_The_Noun_Project.svg).
-"""
+    license_text = (
+        "*License*\n\n"
+        "The code in this repository is licensed under the "
+        "[GNU Affero General Public License v3.0 (AGPL-3.0)](https://www.gnu.org/licenses/agpl-3.0.en.html), "
+        "except where otherwise specified.\n\n"
+        "The icon for the *English Wikipedia Link Converter* Telegram Bot is licensed under a "
+        "[Creative Commons Attribution-ShareAlike 4.0 International License (CC BY-SA 4.0)](http://creativecommons.org/licenses/by-sa/4.0/). "
+        "See the [icon directory](https://github.com/JnTon/English-Wikipedia-Link-Converter-Telegram-Bot/tree/main/Telegram-Bot-Icon) for more details.\n\n"
+        "*Image Credits*\n\n"
+        "The bot's icon incorporates images from the following sources:\n\n"
+        "- *Wikipedia logo*, Version2 by Vanished user 24kwjf10h32h, Version 1 by Nohat (concept by Paullusmagnus); Wikimedia., "
+        "is used under a [Creative Commons Attribution-ShareAlike 3.0 Unported License (CC BY-SA 3.0)](https://creativecommons.org/licenses/by-sa/3.0/) "
+        "and can be found [here on Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Wikipedia-logo-v2-square.svg).\n\n"
+        "- *Left arrow*, by Icons8 is licensed under [CC0](https://creativecommons.org/share-your-work/public-domain/cc0/) "
+        "and is available [here on Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Left-arrow_(61413)_-_The_Noun_Project.svg)."
+    )
     await update.message.reply_text(
         license_text,
         parse_mode='MarkdownV2',
         disable_web_page_preview=True,
         reply_to_message_id=update.message.message_id
-)
-
+    )
+    
 async def async_lambda_handler(event, context):
     """Asynchronous handler for processing Telegram updates."""
     token = os.getenv("YOUR_TELEGRAM_BOT_TOKEN")
